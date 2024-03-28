658430af0f4395b6d7840bf41ef75141ff6035e0
Yushis7 <150568560+Yushis7@users.noreply.github.com>
2024-03-28 20:22:46 +0900

    Add files via upload

---
+++ 크롬만들기/app.js
@@ -0,0 +1,54 @@
+// // alert("hi");
+// const a = 5;
+// const b = 2;
+// const myName = "Yun";
+// // let myName = "yushis";
+// // var myName = "U";
+// //const,let위주로 쓰고 var는 그닥 쓸 필요 없음
+// const amIFat = false;
+// console.log(amIFat); //true,false,null,undefined 4가지 종류 있음
+
+
+
+
+
+// console.log(a+b);
+// console.log("hello" + myName);
+
+// myName = "jun";
+
+// console.log ("my name is + myName");
+
+
+// const daysOfWeek = mon+tue+wed+thu+fri+sat+sun;
+// const daysOfWeek = [mon,tue,wed,thu,fri,sat,sun];
+
+// //// 배열에서 요일 지정하기
+// console.log(daysOfWeek[5]);
+
+// //// 배열에서 요일 추가하기
+// daysOfWeek.push("sun");
+
+// console.log(daysOfWeek);
+
+// const nonsense = [1,2,"hello",false,null,true];
+
+// console.log(nonsense);
+
+// const toBuy = ["potato","tomato","pizza"];
+
+// toBuy.push("cola");
+
+// console.log(toby[1]);
+
+// const playName = "jun";
+// const playerPoints = 100;
+// const playerHandsome = false;
+// const playerfat = "little bit";
+// 일일이 이렇게 쓰지말고
+const play = ["jun",100,false,"littlebit"];
+
+player.name
+player.points 
+player.hansome 
+
