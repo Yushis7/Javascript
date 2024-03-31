183aa5ee16d11c3508d03658ee2b6f86a325006b
Yushis7 <150568560+Yushis7@users.noreply.github.com>
2024-03-31 16:35:53 +0900

    Add files via upload

--- 크롬만들기/app.js
+++ 크롬만들기/app.js
@@ -208,16 +208,183 @@
 //     console.log("You can whatever you want");
 // }
 
-document.title = "Hello.js"
+// document.title = "Hello.js"
 
-//document는 html을 의미함 
-const title = document.getElementById("title");
-title.innerText = "Got you!"; // title로 지정하면 html의 title이 변경 됨
+// //document는 html을 의미함 
+// const title = document.getElementById("title");
+// title.innerText = "Got you!"; // title로 지정하면 html의 title이 변경 됨
 
-console.log(title.id);
-console.log(title.className);
+// console.log(title.id);
+// console.log(title.className);
+
+// const title = document.querySelector("hello"); // querySelector를 많이 씀
+// console.log(title);
+// title.innerText = "Hello";
+
+// title.style.color = "blue";
+
+// const title = document.querySelector("div.hello:first-child h1");
+
+// function handleTitleClick(){
+//     h1.title.style.color = "blue";
+// }
+
+// function handleMouseEnter() {
+//     h1.title.innerText = "Mouse is here!";
+// }
+
+// function handleMouseLeave(){
+//     h1.title.innerText = "Mouse is gone!";
+// }
+// function handleWindowResize(){
+//     document.body.style.backgroundColor = "toamto";
+// }
+// function handleWindowCopy(){
+//     akert("copier!");
+// }
+// function handleWindowOffline(){
+//     alert("offline wifi");
+// }
+// function handleWindowOnline(){
+//     alert("All Good");
+// }
+
+// h1.addEventListener("click",handleTitleClick);
+// h1.addEventListener("mouseenter",handleMouseEnter);
+// h1.addEventListener("mouseleave",handleMouseLeave);
+
+// window.addEventListener("resize",handleWindowResize);
+// window.addEventListener("copy",handleWindowCopy);
+// window.addEventListener("offline",handleWindowOffline);
+// window.addEventListener("online",handleWindowOnline);
+
+// const h1 = document.querySelector("div.hello:first-child h1");
+
+// function handleTitleClick(){
+//     if  (h1.style.color === "blue"){
+//         h1.style.color = "tomato";
+//     } else{
+//         h1.style.color = "blue";
+//     }
+//     // console.log(h1.style.color);
+// }
+
+// h1.addEventListener("click",handleTitleClick);
+// //여기서 h1.style.color 를 일일이 쓰는건 좀 깔끔하지 않으니
+
+// const h1 = document.querySelector("div.hello:first-child h1");
+
+// function handleTitleClick(){
+//     const currentColor = h1.style.color;
+//     let newColor;
+//     if  (currentColor === "blue"){
+//         newColor = "tomato";
+//     } else{
+//         newColor = "blue";
+//     }
+//     h1.style.color = newColor;
+// }
+
+// h1.addEventListener("click",handleTitleClick);
+// //로 바꿔준다
+
+// //css와 연동시켜 바꿀 수 도 있다
+// const h1 = document.querySelector("div.hello:first-child h1");
+
+// function handleTitleClick(){
+//     h1.className = "active"; // active가 css의 active와 연동됨
+// }
+
+// h1.addEventListener("click",handleTitleClick);
+// //로 바꿔준다
 
 
+// // login 만들기 
+// const loginForm = document.getElementById("login-form");
+// const loginInput = loginForm.querySelector(" input");
+// const loginButton = loginForm.querySelector(" button");
+ 
+// // or 
+// const loginInput = document.querySelector("#login-form input");
+// const loginButton = document.querySelector("#login-form button");
+ 
+// function onLoginBtnClick() {
+//     // console.log(loginInput.value);
+//     // console.log("click");
+//     const username = loginInput.value;
+//     if(username ===""){
+//         alert("please write your name");
+//     } else if (username.length > 15) {
+//         alert("your name is too long");
+//     }
+// }
+
+// loginButton.addEventListener("click",onLoginBtnClick);
+// // 근데 굳이 js로 이렇게 코드 칠 필요 없음 html로 쉽게 가능
+
+//html 에 맡기고 버튼만 구현시키기
+
+// const loginInput = document.querySelector("#login-form input");
+// const loginButton = document.querySelector("#login-form button");
+
+// function onLoginBtnClick() {
+//     const username = loginInput.value;
+//     console.log(username);
+// }
+// loginButton.addEventListener("click",onLoginBtnClick);
+
+
+
+//히든으로 아이디 로그인시 로그인 창 없애기
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+const HIDDEN_CLASSNAME = "Hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    // greeting.innerText = "Hello"+username;
+    greeting.innerText = `Hello ${username}`; //347(윗)줄과 같다
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+loginForm.addEventListener("submit",onLoginSubmit);
+
+const savedUsername= localStorage.getItem(USERNAME_KEY);
+
+
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+} else {
+    greeting.innerText = `Hello${savedUsername}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+
+}
+
+
+// const loginInput = document.querySelector("#login-form input");
+// const loginButton = document.querySelector("#login-form button");
+// const HIDDEN_CLASSNAME = "Hidden";
+
+// function onLoginSubmit(event) {
+//     event.preventDefault();
+//     loginForm.classList.add(HIDDEN_CLASSNAME);
+//     const username = loginInput.value;
+//     greeting.innerText = "Hello"+username;
+//     // greeting.innerText = `Hello ${username}`; //347(윗)줄과 같다
+//     greeting.classList.remove(HIDDEN_CLASSNAME);
+//     console.log(loginInput.value);
+// }
+// function handleLinkClick(){
+//     alert("clicked");
+
+// }
+// loginForm.addEventListener("submit",onLoginSubmit);
+// link.addEventListener("click",handleLinkClick);
 
 
 
